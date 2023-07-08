cb09850a36ffbcc6893174e08d83830211b29db3
sabitrrrew <kuzultaevsabit@gmail.com>
2023-07-08 14:24:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import Slider from 'App/Slider'
+import React, { useState } from 'react';
 
 const App = () => {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    const handleInputChange = (event) => {
+        setNewTask(event.target.value);
+    };
+
+    const handleAddTask = () => {
+        if (newTask.trim() !== '') {
+            setTasks([...tasks, newTask]);
+            setNewTask('');
+        }
+    };
+
     return (
-        <div className='App'>
-            <h1>Фото Слайдер</h1>
-            <Slider/>
+        <div>
+            <h1>Todo App</h1>
+            <input type="text" value={newTask} onChange={handleInputChange} />
+            <button onClick={handleAddTask}>Add Task</button>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
         </div>
-    )
-}
+    );
+};
 
 export default App;
